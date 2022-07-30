a646e7bf2cb0fd062f27d03e55911d163f710e9c
lucassbeghen <lucasasbeghen@hotmail.com>
2022-07-30 13:14:21 -0300

    Resolução

--- day1.js
+++ day1.js
@@ -1,4 +1,5 @@
-// Sendo assim, a sua tarefa de hoje é reescrever o código abaixo de maneira que ele imprima as informações de maneira correta, que faça sentido e sem erros:
+// Sendo assim, a sua tarefa de hoje é reescrever o código abaixo de maneira que ele imprima as informações
+//  de maneira correta, que faça sentido e sem erros:
 
 let numeroUm = 1
 let stringUm = '1'
